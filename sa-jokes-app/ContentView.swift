37c8afb50bc5c4ea61b2c8a542bcb2451ddb5aa5
YJ Soon <yjsoon@tinkertanker.com>
2025-07-26 10:20:01 +0800

    Canged button color to a better colour

--- sa-jokes-app/ContentView.swift
+++ sa-jokes-app/ContentView.swift
@@ -47,7 +47,7 @@ struct ContentView: View {
                 } label: {
                     Text("What?")
                         .padding()
-                        .background(Color.blue)
+                        .background(.red)
                         .foregroundStyle(.white)
                 }
                 .padding()
